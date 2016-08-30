055d45f336d7f92972949bcbc2fff37092197c9d
kailashnathan <kailash.united@gmail.com>
2016-08-30 08:36:14 +0530

    Update Max element in a stack.py

--- Max element in a stack.py
+++ Max element in a stack.py
@@ -1,4 +1,10 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+""" You have an empty sequence, and you will be given  queries. Each query is one of these three types:
+
+1 x  -Push the element x into the stack.
+2    -Delete the element present at the top of the stack.
+3    -Print the maximum element in the stack.
+"""
 class Stack:
      def __init__(self):
          self.items = []
